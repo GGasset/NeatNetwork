c3127dbde6a14e78a9a21839392411e8abd142a4
Germán <GassetGerman@gmail.com>
2022-07-15 14:22:43 +0200

    created file with info about gradients

---
+++ NetworkFiles/GradientValues.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeatNetwork.NetworkFiles
+{
+    public class GradientValues
+    {
+        internal List<Point> previousActivationGradientsPosition;
+        internal List<double> previousActivationGradients;
+        internal double[] weightGradients;
+        internal double biasGradient;
+
+        public GradientValues()
+        {
+
+        }
+    }
+}
