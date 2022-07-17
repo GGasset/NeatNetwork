abf142f370b22088e109758834009e6fa3e48b92
Germán <GassetGerman@gmail.com>
2022-07-17 20:30:59 +0200

    Created ValueGeneration.cs, cutted Generate weight to ValueGeneration, also created and finished WillMutate() and GetVariation(), added field to NN and created but left blank evolve function

---
+++ Libraries/ValueGeneration.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeatNetwork.Libraries
+{
+    internal static class ValueGeneration
+    {
+        static int randomI = int.MinValue;
+
+        public static double GenerateWeight(double minValue, double maxValue, double weightClosestTo0)
+        {
+            Random r = new Random(DateTime.Now.Millisecond + randomI++);
+
+            (minValue, maxValue) = (Math.Min(minValue, maxValue), Math.Max(minValue, maxValue));
+
+            double v;
+            // set is negative to -1 or 1
+            int isPositive = r.Next(0, 2);
+            isPositive -= Convert.ToInt32(isPositive == 0);
+
+            //if max value is negative convert is negative to -1
+            isPositive -= 2 * Convert.ToInt32(maxValue < 0);
+            //if min value is positive convert is negative to 1
+            isPositive += 2 * Convert.ToInt32(minValue >= 0);
+
+            weightClosestTo0 = Math.Abs(weightClosestTo0);
+
+            // Set value closest to 0 to the closest value to 0 in respect with min/max value only if both values are positive or negative
+            weightClosestTo0 += (minValue - weightClosestTo0) * Convert.ToInt32(minValue >= 0);
+            weightClosestTo0 -= (weightClosestTo0 - maxValue) * Convert.ToInt32(maxValue < 0);
+
+            v = weightClosestTo0 * isPositive;
+            double randomness = r.NextDouble();
+            // from v which equals WeightClosestTo0 move up to max value or min value depending if its negative
+            v += randomness * (maxValue - weightClosestTo0) * Convert.ToInt32(isPositive == 1);
+            v += randomness * (minValue + weightClosestTo0) * Convert.ToInt32(isPositive == -1);
+
+            return v;
+        }
+
+        public static double GetVariation(double minValue, double maxValue)
+        {
+            (minValue, maxValue) = (Math.Min(minValue, maxValue), Math.Max(minValue, maxValue));
+            double output = new Random(++randomI + DateTime.Now.Millisecond).NextDouble();
+            output *= maxValue - minValue;
+            output += minValue;
+            return output;
+        }
+
+        public static int WillMutate()
+        {
+            return new Random(++randomI + DateTime.Now.Millisecond).Next(0, 2);
+        }
+    }
+}
--- NetworkFiles/NeuronConnectionsInfo.cs
+++ NetworkFiles/NeuronConnectionsInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using NeatNetwork.Libraries;
 
 namespace NeatNetwork.Libraries
 {
@@ -27,7 +28,7 @@ namespace NeatNetwork.Libraries
         /// </summary>
         internal void AddNewConnection(int layerIndex, int neuronIndex, double minValue, double maxValue, double valueClosestTo0)
         {
-            AddNewConnection(layerIndex, neuronIndex, GenerateWeight(minValue, maxValue, valueClosestTo0));
+            AddNewConnection(layerIndex, neuronIndex, ValueGeneration.GenerateWeight(minValue, maxValue, valueClosestTo0));
         }
 
         /// <summary>
@@ -42,9 +43,8 @@ namespace NeatNetwork.Libraries
         internal void AdjustToNewLayerBeingAdded(int layerInsertionIndex, bool isinsertedInPreviousLayer, int insertedLayerLength, double minWeight, double maxWeight, double weightClosestTo0)
         {
             for (int i = 0; i < ConnectedNeuronsPos.Count; i++)
-            {
-                ConnectedNeuronsPos[i].Offset(Convert.ToInt32(ConnectedNeuronsPos[i].X >= layerInsertionIndex), 0);
-            }
+=                ConnectedNeuronsPos[i].Offset(Convert.ToInt32(ConnectedNeuronsPos[i].X >= layerInsertionIndex), 0);
+
 
             if (!isinsertedInPreviousLayer)
                 return;
@@ -53,41 +53,6 @@ namespace NeatNetwork.Libraries
             {
                 AddNewConnection(layerInsertionIndex, i, minWeight, maxWeight, weightClosestTo0);
             }
-        }
-
-
-        static int randomI = int.MinValue;
-
-        public static double GenerateWeight(double minValue, double maxValue, double weightClosestTo0)
-        {
-            Random r = new Random(DateTime.Now.Millisecond + randomI);
-            randomI++;
-
-            (minValue, maxValue) = (Math.Min(minValue, maxValue), Math.Max(minValue, maxValue));
-
-            double v;
-            // set is negative to -1 or 1
-            int isPositive = r.Next(0, 2);
-            isPositive -= Convert.ToInt32(isPositive == 0);
-
-            //if max value is negative convert is negative to -1
-            isPositive -= 2 * Convert.ToInt32(maxValue < 0);
-            //if min value is positive convert is negative to 1
-            isPositive += 2 * Convert.ToInt32(minValue >= 0);
-
-            weightClosestTo0 = Math.Abs(weightClosestTo0);
-
-            // Set value closest to 0 to the closest value to 0 in respect with min/max value only if both values are positive or negative
-            weightClosestTo0 += (minValue - weightClosestTo0) * Convert.ToInt32(minValue >= 0);
-            weightClosestTo0 -= (weightClosestTo0 - maxValue) * Convert.ToInt32(maxValue < 0);
-
-            v = weightClosestTo0 * isPositive;
-            double randomness = r.NextDouble();
-            // from v which equals WeightClosestTo0 move up to max value or min value depending if its negative
-            v += randomness * (maxValue - weightClosestTo0) * Convert.ToInt32(isPositive == 1);
-            v += randomness * (minValue + weightClosestTo0) * Convert.ToInt32(isPositive == -1);
-
-            return v;
         }
     }
 }
