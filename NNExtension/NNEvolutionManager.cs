f309c84dd6bdb9b7887a0662902130301229a0ba
Germán <GassetGerman@gmail.com>
2022-07-20 13:08:16 +0200

    added functions to set network near last scored networks Finished HaveChild() function

--- NNExtension/NNEvolutionManager.cs
+++ NNExtension/NNEvolutionManager.cs
@@ -16,6 +16,9 @@ namespace NeatNetwork
             double mutationChance = .1, double fieldMaxMutation = .1, double initialMaxMutationValue = .27, double newNeuronChance = .04, double newLayerChance = .01,
             double initialValueForMaxMutation = .27, double maxMutationOfMutationValues = .2, double maxMutationOfMutationValueOfMutationValues = .05)
         {
+            MaxScore = double.MaxValue;
+            MinScore = double.MinValue;
+
             Networks = new List<NN>();
             Scores = new List<double>();
             for (int i = 0; i < startingNetworkCount; i++)
@@ -37,7 +40,7 @@ namespace NeatNetwork
             for (int i = 0; i < Scores.Count; i++)
             {
                 double score = Scores[i];
-                double currentChildCount = Math.Ceiling(score / MaxScore * maxChilds);
+                double currentChildCount = Math.Ceiling((score - MinScore) / (MaxScore - MinScore) * maxChilds);
                 currentChildCount = Math.Min(currentChildCount, minChilds);
 
                 for (int j = 0; j < currentChildCount; j++)
@@ -50,7 +53,7 @@ namespace NeatNetwork
         }
 
         /// <summary>
-        /// 
+        /// Don't use really large negative or positive numbers for correct functioning
         /// </summary>
         /// <param name="score">Must be positive to properly work</param>
         internal void SetNextNetworkToBeScoredScore(double score)
@@ -58,6 +61,7 @@ namespace NeatNetwork
             score = double.MaxValue / 4 + score;
             Scores.Add(score);
             MaxScore += (score - MaxScore) * Convert.ToInt32(score > MaxScore);
+            MinScore += (score - MinScore) * Convert.ToInt32(score < MinScore);
             MaxScoredNetwork += (Scores.Count - MaxScoredNetwork) * Convert.ToInt32(score > MaxScore);
         }
 
@@ -82,6 +86,16 @@ namespace NeatNetwork
         internal void SetNetwork(NN n, int index)
         {
             Networks[index] = n;
+        }
+
+        internal void SetNextToScoreNetwork(NN n)
+        {
+            Networks[Scores.Count] = n;
+        }
+
+        internal void SetLastScoredNetwork(NN n)
+        {
+            Networks[Scores.Count - 1] = n;
         }
     }
 }
