172567546ffb8705e2111f600552baad61a7fddd
Germán <GassetGerman@gmail.com>
2022-07-21 21:00:19 +0200

    added LSTMNeuron and RecurentNeuron

---
+++ NetworkFiles/LSTMNeuron.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeatNetwork.Libraries;
+using NeatNetwork.NetworkFiles;
+
+namespace NeatNetwork.NetworkFiles
+{
+    internal class LSTMNeuron
+    {
+        private double CellState;
+        private double HiddentState;
+
+        NeuronConnectionsInfo weights;
+    }
+}
---
+++ NetworkFiles/RecurentNeuron.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeatNetwork.NetworkFiles
+{
+    internal class RecurentNeuron
+    {
+        internal readonly NeuronType neuronType;
+        internal Neuron Neuron;
+
+        public enum NeuronType
+        {
+            Neuron,
+            LSTM,
+            Recurrent
+        }
+    }
+}
