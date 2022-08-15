4fb62c24df553dd5f1aa305b51a3393bb43393d3
Germán Gasset <gassetgerman@gmail.com>
2022-08-15 20:19:27 +0200

    prepared infrastructure for RNN

---
+++ NetworkFiles/NeuronHolder.cs
@@ -0,0 +1,42 @@
+﻿using NeatNetwork.Libraries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeatNetwork.NetworkFiles
+{
+    internal class NeuronHolder
+    {
+        public readonly NeuronType neuronType;
+        internal Neuron Neuron;
+        internal LSTMNeuron LSTMNeuron;
+
+        internal double Execute(List<double[]> previousNeuronsActivations, Activation.ActivationFunctions activationFunction, out NeuronValues outputVals)
+        {
+            double activation;
+            switch (neuronType)
+            {
+                case NeuronType.Neuron:
+                    activation = Neuron.Execute(previousNeuronsActivations, activationFunction, out double linearFunction);
+                    break;
+                case NeuronType.LSTM:
+
+                    break;
+                case NeuronType.Recurrent:
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+            return activation;
+        }
+
+        public enum NeuronType
+        {
+            Neuron,
+            LSTM,
+            Recurrent
+        }
+    }
+}
---
+++ NetworkFiles/NeuronValues.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeatNetwork.NetworkFiles
+{
+    public class NeuronValues
+    {
+        internal readonly NeuronHolder.NeuronType NeuronType;
+        internal double Activation;
+        internal double LinearFunction;
+        internal double HiddenState;
+        internal double CellState;
+
+        internal NeuronValues(NeuronHolder.NeuronType neuronType)
+        {
+            NeuronType = neuronType;
+            Activation = 0;
+            LinearFunction = 0.0;
+            HiddenState = 0.0;
+            CellState = 0.0;
+        }
+    }
+}
