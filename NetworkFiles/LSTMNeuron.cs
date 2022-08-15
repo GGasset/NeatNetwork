49ca3c398a3bb750168d4e5b85bdee6fc77d9a7f
Germán Gasset <gassetgerman@gmail.com>
2022-08-15 21:51:48 +0200

    continued with infrastructure for RNN Started LSTMNeuron.Execute()

--- NetworkFiles/LSTMNeuron.cs
+++ NetworkFiles/LSTMNeuron.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,27 @@ namespace NeatNetwork.NetworkFiles
 {
     internal class LSTMNeuron
     {
-        private double CellState;
-        private double HiddentState;
+        internal double CellState;
+        internal double HiddentState;
 
-        NeuronConnectionsInfo weights;
+        internal NeuronConnectionsInfo weights;
+
+        internal double ForgetWeight;
+        internal double StoreWeight;
+        internal double OutputWeight;
+
+        internal double Execute(List<double[]> previousLayerActivations, Activation.ActivationFunctions activationFunction, out NeuronValues neuronVals)
+        {
+            neuronVals = new NeuronValues(NeuronHolder.NeuronType.LSTM);
+
+            double linearFunction = 0;
+            for (int i = 0; i < weights.Length; i++)
+            {
+                Point currentConnectedPos = weights.ConnectedNeuronsPos[i];
+                linearFunction += previousLayerActivations[currentConnectedPos.X][currentConnectedPos.Y] * weights.Weights[i];
+            }
+            neuronVals.LinearFunction = linearFunction;
+            CellState += linearFunction;
+        }
     }
 }
--- NetworkFiles/NeuronValues.cs
+++ NetworkFiles/NeuronValues.cs
@@ -9,10 +9,22 @@ namespace NeatNetwork.NetworkFiles
     public class NeuronValues
     {
         internal readonly NeuronHolder.NeuronType NeuronType;
+
+        // All neuron types
         internal double Activation;
         internal double LinearFunction;
+
+        // Recurrent and LSTM
         internal double HiddenState;
+
+        // LSTM
         internal double CellState;
+        internal double AfterForgetGateBeforeForgetWeight;
+        internal double AfterForgetGateAfterForgetWeight;
+        internal double AfterSigmoidStoreGateBeforeStoreWeight;
+        internal double AfterSigmoidStoreGateAfterStoreWeight;
+        internal double AfterTanhStoreGate;
+        internal double AfterStoreGateMultiplication;
 
         internal NeuronValues(NeuronHolder.NeuronType neuronType)
         {
