4e6666466e006c80edbfd763c130bc139aac59a6
Germán <GassetGerman@gmail.com>
2022-08-18 21:17:42 +0200

    added RNN.Execute()

--- RNN.cs
+++ RNN.cs
@@ -8,13 +8,42 @@ using static NeatNetwork.Libraries.Activation;
 
 namespace NeatNetwork
 {
-    internal class RNN
+    public class RNN
     {
         public int Length => Neurons.Count;
 
         internal ActivationFunctions ActivationFunction;
         internal List<List<NeuronHolder>> Neurons;
 
-        
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="networkExecutionValues"></param>
+        /// <returns></returns>
+        public double[] Execute(double[] input, out List<List<NeuronValues>> networkExecutionValues)
+        {
+            networkExecutionValues = new List<List<NeuronValues>>();
+            List<double[]> neuronActivations = new List<double[]>()
+            {
+                input
+            };
+
+            for (int i = 0; i < Length; i++)
+            {
+                networkExecutionValues.Add(new List<NeuronValues>());
+
+                int layerLength = Neurons[i].Count;
+                neuronActivations.Add(new double[layerLength]);
+
+                for (int j = 0; j < layerLength; j++)
+                {
+                    neuronActivations[i + 1][j] = Neurons[i][j].Execute(neuronActivations, ActivationFunction, out NeuronValues neuronExecutionValues);
+                    networkExecutionValues[i].Add(neuronExecutionValues);
+                }
+            }
+
+            return neuronActivations[neuronActivations.Count - 1];
+        }
     }
 }
