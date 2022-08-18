f94a2d9a9ccc1b85e0b42dbaa7d1a84a1772b570
Germán <GassetGerman@gmail.com>
2022-08-18 20:55:33 +0200

    created and declared fields at RNN.cs

---
+++ RNN.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeatNetwork.NetworkFiles;
+using static NeatNetwork.Libraries.Activation;
+
+namespace NeatNetwork
+{
+    internal class RNN
+    {
+        public int Length => Neurons.Count;
+
+        internal ActivationFunctions ActivationFunction;
+        internal List<List<NeuronHolder>> Neurons;
+
+        
+    }
+}
