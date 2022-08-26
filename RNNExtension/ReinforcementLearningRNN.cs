767dea8c5615ad64dc4fa3d68db3f0ed76e015ff
Germán <GassetGerman@gmail.com>
2022-08-26 15:21:16 +0200

    added fields and .Execute to ReinforcementLearningRNN.cs

--- RNNExtension/ReinforcementLearningRNN.cs
+++ RNNExtension/ReinforcementLearningRNN.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NeatNetwork.NetworkFiles;
 
 namespace NeatNetwork
 {
     internal class ReinforcementLearningRNN
     {
+        public RNN n;
+        List<double[]> Inputs;
+        List<List<NeuronExecutionValues[]>> neuronExecutionValues;
+        List<List<double[]>> neuronOutputs;
+
+        List<double> RewardHistory;
+        double CurrentDefaultReward;
+
+        public double[] Execute(double[] input)
+        {
+            var output = n.Execute(input, out List<NeuronExecutionValues[]> networkExecutionValues, out List<double[]> neuronExecutionOutputs);
+            neuronExecutionValues.Add(networkExecutionValues);
+            neuronOutputs.Add(neuronExecutionOutputs);
+            RewardHistory.Add(CurrentDefaultReward);
+
+            return output;
+        }
     }
 }
