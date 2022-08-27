a5e700845f27eab3cc88217af9a6b9004ab29c30
Germán <GassetGerman@gmail.com>
2022-08-27 14:29:35 +0200

    added Reinforcement learning to program

--- Program.cs
+++ Program.cs
@@ -12,7 +12,29 @@ namespace NeatNetwork
     {
         static void Main(string[] args)
         {
-            RNN n = new RNN(new int[] { 1, 13, 5, 1 }, new NeuronTypes[] { NeuronTypes.LSTM, NeuronTypes.LSTM, NeuronTypes.LSTM }, Activation.ActivationFunctions.Sigmoid);
+
+            RNN rNN = new RNN(new int[] { 1, 13, 5, 1 }, new NeuronTypes[] { NeuronTypes.LSTM, NeuronTypes.LSTM, NeuronTypes.LSTM }, Activation.ActivationFunctions.Sigmoid);
+            ReinforcementLearningRNN n = new ReinforcementLearningRNN(rNN);
+            var X = new double[] { 5 };
+            var expectedFirstValue = 0.5;
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                var firstOutput = n.Execute(X)[0];
+                var distanceToTarget = Math.Abs(firstOutput - expectedFirstValue + 1);
+
+                var firstReward = 1 / (distanceToTarget);
+
+                n.GiveReward(firstReward);
+
+                var secondOutput = n.Execute(X)[0];
+                n.GiveReward(secondOutput - firstOutput);
+
+                n.TerminateAgent();
+
+                Console.WriteLine($"{firstOutput}\n{secondOutput}\n----------------");
+            }
+
+            /*RNN n = new RNN(new int[] { 1, 13, 5, 1 }, new NeuronTypes[] { NeuronTypes.LSTM, NeuronTypes.LSTM, NeuronTypes.LSTM }, Activation.ActivationFunctions.Sigmoid);
             List<List<double[]>> X = new List<List<double[]>>()
             {
                 new List<double[]>()
@@ -53,65 +75,65 @@ namespace NeatNetwork
 
             n.DeleteMemory();
 
-            Console.ReadLine();
+            Console.ReadLine();*/
 
 
-             /*List<double[]> X = new List<double[]>()
-             {----------
-                              new double[] { 0,0 },
-                 new double[] { 1,0 },
-                 new double[] { 0,1 },
-                 new double[] { 1,1 },
-             };
+            /*List<double[]> X = new List<double[]>()
+            {----------
+                             new double[] { 0,0 },
+                new double[] { 1,0 },
+                new double[] { 0,1 },
+                new double[] { 1,1 },
+            };
 
-             List<double[]> y = new List<double[]>()
-             {
-                 new double[] { 1 },
-                 new double[] { -1 },
-                 new double[] { -1 },
-                 new double[] { 1 },
-             };
-
-
-            // NN Evolution Learning demonstration
-            NNEvolutionManager world = new NNEvolutionManager(15000, new int[] { 2, 1 }, Activation.ActivationFunctions.Sigmoid, 1.5, -1.5, 0.37, 1, .25);
-
-
-            for (int i = 0; i < 4000; i++)
+            List<double[]> y = new List<double[]>()
             {
-                int j = 0;
-                while (!world.AreAllNetworksScored())
-                {
-                    NN cn = world.GetNextToScoreNetwork();
-                    double meanCost = 0;
-                    for (int k = 0; k < X.Count; k++)
-                    {
-                        meanCost += Cost.GetCost(cn.Execute(X[k]), y[k], Cost.CostFunctions.SquaredMean);
-                    }
-                    meanCost /= X.Count;
+                new double[] { 1 },
+                new double[] { -1 },
+                new double[] { -1 },
+                new double[] { 1 },
+            };
 
-                    if (i != 0 && j != 0)
-                        world.SetNextNetworkToBeScoredScore(-meanCost);
-                    else
-                        world.SetFirstNetworkScore(-meanCost);
 
-                    /*double score = cn.Execute(X[1])[0];
-                    world.SetNextNetworkToBeScoredScore(score);*
-                    j++;
-                }
-                world.DropWorstNetworks(.5);
-                Console.WriteLine($"Generation: {i}   NetworkCount: {world.Networks.Count}   MaxScore: {world.MaxScore}");
+           // NN Evolution Learning demonstration
+           NNEvolutionManager world = new NNEvolutionManager(15000, new int[] { 2, 1 }, Activation.ActivationFunctions.Sigmoid, 1.5, -1.5, 0.37, 1, .25);
 
-                world.HaveChild(1, 2);
-            }
-            Console.WriteLine("\n");
-            NN n = world.GetMaxScoredNetwork();
-            for (int i = 0; i < X.Count; i++)
-            {
-                double output = n.Execute(X[i])[0];
-                double expected = y[i][0];
-                Console.WriteLine($"Output: {output}, Expected: {expected}");
-            }*/
+
+           for (int i = 0; i < 4000; i++)
+           {
+               int j = 0;
+               while (!world.AreAllNetworksScored())
+               {
+                   NN cn = world.GetNextToScoreNetwork();
+                   double meanCost = 0;
+                   for (int k = 0; k < X.Count; k++)
+                   {
+                       meanCost += Cost.GetCost(cn.Execute(X[k]), y[k], Cost.CostFunctions.SquaredMean);
+                   }
+                   meanCost /= X.Count;
+
+                   if (i != 0 && j != 0)
+                       world.SetNextNetworkToBeScoredScore(-meanCost);
+                   else
+                       world.SetFirstNetworkScore(-meanCost);
+
+                   /*double score = cn.Execute(X[1])[0];
+                   world.SetNextNetworkToBeScoredScore(score);*
+                   j++;
+               }
+               world.DropWorstNetworks(.5);
+               Console.WriteLine($"Generation: {i}   NetworkCount: {world.Networks.Count}   MaxScore: {world.MaxScore}");
+
+               world.HaveChild(1, 2);
+           }
+           Console.WriteLine("\n");
+           NN n = world.GetMaxScoredNetwork();
+           for (int i = 0; i < X.Count; i++)
+           {
+               double output = n.Execute(X[i])[0];
+               double expected = y[i][0];
+               Console.WriteLine($"Output: {output}, Expected: {expected}");
+           }*/
 
             // NN Supervised learning demonstration
             /*NN world = new NN(new int[] { 4, 5, 6, 5, 4, 3, 1 }, Activation.ActivationFunctions.Sigmoid, 1.5, -1.5, .5);
@@ -144,7 +166,7 @@ namespace NeatNetwork
 
             Console.WriteLine($"{world.Execute(X[0])[0]}");
             Console.ReadKey();*/
-            
+
             // NN Reinforcement Learning demonstration
             /*double learningRate = 1;
             ReinforcementLearningNN agent = new ReinforcementLearningNN(new NN(new int[] { 1, 15, 4, 400, 1 }, Activation.ActivationFunctions.Sigmoid), learningRate);
