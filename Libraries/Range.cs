769c8657f048bfc8e795c48e61124b24204642ff
Germán <GassetGerman@gmail.com>
2022-08-28 20:57:16 +0200

    Added files for RNN groupings

---
+++ Libraries/Range.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeatNetwork.Libraries
+{
+    internal class Range
+    {
+        public int FromI;
+        public int ToI;
+
+        public Range(int from, int to)
+        {
+            FromI = from;
+            ToI = to;
+        }
+    }
+}
