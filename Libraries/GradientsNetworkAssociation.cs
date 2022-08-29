5eec1e43020524e9c60448e91b5a77eda0ffce41
Germán <GassetGerman@gmail.com>
2022-08-29 23:08:51 +0200

    added and finished GradientsNetworkAssociation.cs

---
+++ Libraries/GradientsNetworkAssociation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NeatNetwork.NetworkFiles;
+
+namespace NeatNetwork.Libraries
+{
+    internal class GradientsNetworkAssociation
+    {
+        internal List<List<NeuronHolder>> Gradients;
+        internal int NetworkIndex;
+
+        internal GradientsNetworkAssociation(List<List<NeuronHolder>> gradients, int networkIndex)
+        {
+            Gradients = gradients;
+            NetworkIndex = networkIndex;
+        }
+    }
+}
