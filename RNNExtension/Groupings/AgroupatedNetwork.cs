2faf0d9eb500da1e29cb07d600a3d1daabd1ef3b
Germán <GassetGerman@gmail.com>
2022-08-30 01:45:17 +0200

    added RNN n field, Implemented AgroupatedNetwork.ClearInput()

--- RNNExtension/Groupings/AgroupatedNetwork.cs
+++ RNNExtension/Groupings/AgroupatedNetwork.cs
@@ -8,7 +8,13 @@ namespace NeatNetwork.Groupings
 {
     internal class AgroupatedNetwork
     {
-        List<double> input;
-        NetworkConnectionsInfo connections;
+        internal List<double> input;
+        internal List<Connection> connections;
+        public RNN n;
+
+        public List<int> networksConnectedToThis { get; private set; }
+
+
+        internal void ClearInput() => input = new List<double>(new double[n.InputLength]);
     }
 }
