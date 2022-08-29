69ff74bf45d242a35358ae98baea4b8304a75809
Germán <GassetGerman@gmail.com>
2022-08-30 01:13:54 +0200

    added Range.WholeRange, Range == and !=, Range.GetHashCode() and Range.Equals()

--- Libraries/Range.cs
+++ Libraries/Range.cs
@@ -6,15 +6,44 @@ using System.Threading.Tasks;
 
 namespace NeatNetwork.Libraries
 {
-    internal class Range
+    public class Range
     {
         public int FromI;
         public int ToI;
+
+        private int HashCode;
+        private static int NextHashCode = 0;
 
         public Range(int from, int to)
         {
             FromI = from;
             ToI = to;
+
+            HashCode = NextHashCode;
+            NextHashCode++;
+        }
+
+        public Range WholeRange => new Range(0, -1);
+
+        public static bool operator ==(Range a, Range b) => a.FromI == b.FromI && a.ToI == b.ToI;
+        public static bool operator !=(Range a, Range b) => !(a == b);
+
+        public override int GetHashCode()
+        {
+            return HashCode;
+        }
+
+        public override bool Equals(object obj)
+        {
+            try
+            {
+                Range toCompare = (Range)obj;
+                return toCompare == this;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
     }
 }
