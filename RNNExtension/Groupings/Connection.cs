596982314cc640369e28baa918c1bba85ee4a55f
Germán <GassetGerman@gmail.com>
2022-08-30 01:32:00 +0200

    Renamed NetworkConnectionsInfo.cs to Connection.cs

--- RNNExtension/Groupings/Connection.cs
+++ RNNExtension/Groupings/Connection.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NeatNetwork.Libraries;
 
 namespace NeatNetwork.Groupings
 {
-    internal class NetworkConnectionsInfo
+    public class Connection
     {
+        internal List<Range> InputRanges;
+        
     }
 }
