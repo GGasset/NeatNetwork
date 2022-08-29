678ccb38b46c283f698bd628aa2435531f9eecf8
Germán <GassetGerman@gmail.com>
2022-08-30 01:22:08 +0200

    added NetworkGroup.cs and declared  NetworkGroup.ClearInputForNetwork()

---
+++ RNNExtension/Groupings/NetworkGroup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeatNetwork.RNNExtension.Groupings
+{
+    public class NetworkGroup
+    {
+
+        internal void ClearInputForNetwork(int i)
+        {
+
+        }
+    }
+}
