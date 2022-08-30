9518dd19759a2004ea9f824aefa5436c0cd9cae3
Germán <GassetGerman@gmail.com>
2022-08-30 23:48:54 +0200

    Added AgroupatedNetwork constructor and AgroupatedNetwork.Connect()

--- RNNExtension/Groupings/AgroupatedNetwork.cs
+++ RNNExtension/Groupings/AgroupatedNetwork.cs
@@ -3,17 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NeatNetwork.Libraries;
 
 namespace NeatNetwork.Groupings
 {
     internal class AgroupatedNetwork
     {
         private List<double> input;
-        internal List<Connection> connections;
+        internal List<Connection> Connections;
         public RNN n;
 
-        public List<int> ConnectedNetworksI { get; private set; }
+        internal AgroupatedNetwork(RNN n)
+        {
+            this.n = n;
+            Connections = new List<Connection>();
+            ClearInput();
+        }
 
+        internal void Connect(int connectedNetworkI, int connectedNetworkOutputLength, Range inputRange, Range outputRange, double maxWeight, double minWeight, double weightClosestTo0)
+        {
+            var connection = new Connection(inputRange, connectedNetworkI, outputRange, n.InputLength, connectedNetworkOutputLength, maxWeight, minWeight, weightClosestTo0);
+            for (int i = 0; i < Connections.Count; i++)
+            {
+                if (Connections[i].ConnectedNetworkI >= connectedNetworkI)
+                {
+                    Connections.Insert(i, connection);
+                    return;
+                }
+            }
+            Connections.Add(connection);
+        }
 
         internal void ClearInput() => input = new List<double>(new double[n.InputLength]);
     }
