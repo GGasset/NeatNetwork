587f7304c7ba581bed054a8d02748d18c68412bf
Germán <GassetGerman@gmail.com>
2022-08-31 13:40:18 +0200

    Continued on NetworkGroup.GetGradients()

--- RNNExtension/Groupings/NetworkGroup.cs
+++ RNNExtension/Groupings/NetworkGroup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NeatNetwork.NetworkFiles;
+using NeatNetwork.Libraries;
 
 namespace NeatNetwork.Groupings
 {
@@ -22,26 +23,55 @@ namespace NeatNetwork.Groupings
         /// <summary>
         /// 
         /// </summary>
-        /// <param name="costGradients">upper list is in order of execution</param>
-        /// <param name="executionValues">upper list is in order of execution</param>
-        /// <param name="neuronActivations">upper list is in order of execution</param>
+        /// <param name="costGradients">upper list is cronologically ordered containing list of executions in execution order</param>
+        /// <param name="executionValues">upper list is cronologically ordered containing list of executions in execution order</param>
+        /// <param name="neuronActivations">upper list is cronologically ordered containing list of executions in execution order</param>
         /// <returns></returns>
         public List<List<List<NeuronHolder>>> GetGradients(List<List<double[]>> costGradients, List<List<List<NeuronExecutionValues[]>>> executionValues, List<List<List<double[]>>> neuronActivations)
         {
-            int tCount = costGradients[0].Count;
+            int tSCount = costGradients.Count;
 
-            // NetworkOutputGradients is a list representing execution order containing other list representing time containing a cost array
-            List<List<double[]>> networkOutputGradients = new List<List<double[]>>
+            // NetworkOutputGradients is a list representing execution order containing other list representing time containing a cost arrays
+            List<List<double[]>> networksOutputCostGradients = new List<List<double[]>>();
+            for (int i = 0; i < ExecutionOrder.Count; i++)
+            {
+                networksOutputCostGradients.Add(new List<double[]>());
+                for (int t = 0; t < tSCount; t++)
+                {
+                    networksOutputCostGradients[i].Add(new double[Networks[ExecutionOrder[i]].n.OutputLength]);
+                }
+            }
+            
 
             for (int i = ExecutionOrder.Count - 1; i >= 0; i--)
             {
+                int currentExecutionIndex = ExecutionOrder[i];
+
+                List<double[]> currentCostGradients = new List<double[]>();
+                List < List<NeuronExecutionValues[]>> currentExecutionValues = new List<List<NeuronExecutionValues[]>>();
+                List < List<double[]>> currentNeuronActivations = new List<List<double[]>>();
+
+                for (int t = 0; t < tSCount; t++)
+                {
+                    currentCostGradients.Add(costGradients[t][currentExecutionIndex]);
+                    currentExecutionValues.Add(executionValues[t][currentExecutionIndex]);
+                    currentNeuronActivations.Add(neuronActivations[t][currentExecutionIndex]);
+                }
+
                 AgroupatedNetwork cNetwork = Networks[ExecutionOrder[i]];
 
                 cNetwork.n.GetGradients(costGradients[i], executionValues[i], neuronActivations[i], out List<List<double>> inputGradients);
 
                 foreach (var connection in cNetwork.Connections)
                 {
+                    int currentProcessedInputToI = connection.InputRange.ToI;
+                    currentProcessedInputToI += (cNetwork.n.InputLength + 1) * Convert.ToInt32(connection.InputRange == Range.WholeRange);
+                    int inputFromI = connection.InputRange.FromI;
 
+                    for (int j = inputFromI; j < currentProcessedInputToI; j++)
+                    {
+
+                    }
                 }
             }
         }
