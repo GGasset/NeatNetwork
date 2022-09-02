8eb593f4e784bd821f4b0b5ffab35f6e55a742b4
Germán <GassetGerman@gmail.com>
2022-09-03 01:22:01 +0200

    Renamed on Connection.cs also had effect on NetworkGroup.cs

--- RNNExtension/Groupings/NetworkGroup.cs
+++ RNNExtension/Groupings/NetworkGroup.cs
@@ -201,7 +201,7 @@ namespace NeatNetwork.Groupings
 
         internal void PassOutput(double[] networkOutput, Connection connection)
         {
-            Range inputRange = connection.InputRange, outputRange = connection.ConnectedOutputRange;
+            Range inputRange = connection.NetworkInputRange, outputRange = connection.ConnectedOutputRange;
             for (int networkInputI = inputRange.FromI; networkInputI < inputRange.ToI; networkInputI++)
             {
                 for (int inputI = outputRange.FromI; inputI < outputRange.ToI; inputI++)
