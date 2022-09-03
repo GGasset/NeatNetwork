3bec637d4e88ff7f777e818df4ccf65396bec4bb
Germán <GassetGerman@gmail.com>
2022-09-03 16:16:48 +0200

    deleted AgroupatedNetwork.SubtractGradients()

--- RNNExtension/Groupings/AgroupatedNetwork.cs
+++ RNNExtension/Groupings/AgroupatedNetwork.cs
@@ -59,17 +59,6 @@ namespace NeatNetwork.Groupings
 
         internal void ClearInput() => input = new List<double>(new double[n.InputLength]);
 
-        #region Gradient learning
-
-        internal void SubtractGrads(List<List<NeuronHolder>> networkGradients, List<Connection> connectionsGradients, double learningRate)
-        {
-            n.SubtractGrads(networkGradients, learningRate);
-            for (int i = 0; i < Connections.Count; i++)
-                Connections[i].SubtractGrads(connectionsGradients[i], learningRate);
-        }
-
-        #endregion
-
         // TODO: implement optimized search
         public bool IsConnectedTo(int networkI)
         {
