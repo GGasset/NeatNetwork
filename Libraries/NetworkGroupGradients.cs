03724d969c73503917c0a7ca1bd80a820d5676b8
Germán <GassetGerman@gmail.com>
2022-09-03 14:11:40 +0200

    finished NetworkGroupGradiensts.cs

--- Libraries/NetworkGroupGradients.cs
+++ Libraries/NetworkGroupGradients.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NeatNetwork.Groupings;
+using NeatNetwork.NetworkFiles;
 
 namespace NeatNetwork.Libraries
 {
-    internal class NetworkGroupGradientsS
+    internal class NetworkGroupGradients
     {
+        internal List<int> ExecutionOrder;
+        internal List<List<Connection>> outputConnectionsGradients;
+        internal List<List<List<Connection>>> ConnectionsGradients;
+        internal List<List<List<NeuronHolder>>> NetworksGradients;
     }
 }
