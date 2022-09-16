dc05fa458d8a3c3d367b10f9f5df6071ea39e59a
Germán <GassetGerman@gmail.com>
2022-09-17 01:37:28 +0200

    Added DataManipulation ShuffleData() and SliceData

--- Libraries/DataManipulation.cs
+++ Libraries/DataManipulation.cs
@@ -8,6 +8,46 @@ namespace NeatNetwork.Libraries
 {
     public static class DataManipulation
     {
+        public static (List<double[]> shuffledX, List<double[]> shuffledY) ShuffleData(List<double[]> X, List<double[]> y)
+        {
+            var shuffledX = new List<double[]>(X.ToArray());
+            var shuffledY = new List<double[]>(y.ToArray());
 
+            Random r = new Random(DateTime.Now.Millisecond);
+            for (int i = 0; i < shuffledX.Count; i++)
+            {
+                int interchangeI = r.Next(X.Count);
+                (double[] currentX, double[] currentY) = (shuffledX[i], shuffledY[i]);
+                (shuffledX[i], shuffledY[i]) = (shuffledX[interchangeI], shuffledY[interchangeI]);
+                (shuffledX[interchangeI], shuffledY[interchangeI]) = (currentX, currentY);
+            }
+
+            return (shuffledX, shuffledY);
+        }
+
+        public static ((List<double[]> firstX, List<double[]> firstY), (List<double[]> secondX, List<double[]> secondY)) SliceData(List<double[]> X, List<double[]> y, double slicePos = 0.8)
+        {
+            slicePos /= 1 + (X.Count - 1) * Convert.ToInt32(slicePos > 1);
+
+            int dataPartitionI = Convert.ToInt32(X.Count * slicePos);
+            List<double[]> firstX, firstY;
+            firstX =  new List<double[]>();
+            firstY = new List<double[]>();
+            for (int i = 0; i < dataPartitionI; i++)
+            {
+                firstX.Add(X[i]);
+                firstY.Add(y[i]);
+            }
+
+            List<double[]> secondX, secondY;
+            secondX = new List<double[]>();
+            secondY = new List<double[]>();
+            for (int i = dataPartitionI; i < X.Count; i++)
+            {
+                secondX.Add(X[i]);
+                secondY.Add(y[i]);
+            }
+            return ((firstX, firstY), (secondX, secondY));
+        }
     }
 }
